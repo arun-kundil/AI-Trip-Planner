b099e1f485c4142fce4207b3312dac9da3acebfe
arun-kundil <arunkkd21@gmail.com>
2026-05-10 23:13:56 +0530

    Streamlit UI fixes

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,23 +1,30 @@
 from utils.model_loader import ModelLoader # for loading the language model
 from prompt_library.prompt import SYSTEM_PROMPT
-# from tools.weather_info_tool import WeatherInfoTool
-# from tools.place_search_tool import PlaceSearchTool
-# from tools.calculator_tool import CalculatorTool
-# from tools.currency_conversion_tool import CurrencyConversionTool
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConversionTool
 
 ##Langgraph imports
 from langgraph.graph import StateGraph, MessagesState, END, START
-from langgraph.prebuilt import ToolMode, tools_condition
+from langgraph.prebuilt import ToolNode, tools_condition
 
 
 class GraphBuilder():
-    def __init__(self):
-        self.tools = [
-            # WeatherInfoTool(),
-            # PlaceSearchTool(),
-            # CalculatorTool(),
-            # CurrencyConversionTool(),
-        ]
+    def __init__(self, model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        self.tools = []
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tool = PlaceSearchTool()
+        self.calculator_tool = CalculatorTool()
+        self.currency_conversion_tool = CurrencyConversionTool()
+        self.tools.extend([* self.weather_tools.weather_tool_list,
+                           * self.place_search_tool.place_search_tool_list,
+                           * self.calculator_tool.calculator_tool_list,
+                           * self.currency_conversion_tool.currency_conversion_tool_list])
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        self.graph = None
         self.system_prompt = SYSTEM_PROMPT
 
     def agent_function(self,state : MessagesState):
@@ -30,7 +37,7 @@ class GraphBuilder():
     def build_graph(self):
         graph_builder = StateGraph(MessagesState)
         graph_builder.add_node("agent", self.agent_function)
-        graph_builder.add_node("tools", ToolMode(tools=self.tools))
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
         graph_builder.add_edge(START, "agent")
         graph_builder.add_conditional_edges("agent", tools_condition)
         graph_builder.add_edge("agent", "tools")
---
+++ tools/weather_info_tool.py
@@ -0,0 +1,5 @@
+import os
+from typing import Any, Dict, Optional, List
+from dotenv import load_dotenv
+#from utils.weather_info import WeatherForecastTool
+
