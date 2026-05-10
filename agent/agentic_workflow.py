9211a43d23a5848a053bd937d55b089fbc6b3029
arun-kundil <arunkkd21@gmail.com>
2026-05-10 18:46:58 +0530

    Code upated for loading the model

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,24 @@
+from utils.model_loader import ModelLoader # for loading the language model
+from prompt_library.prompt import SYSTEM_PROMPT
+# from tools.weather_info_tool import WeatherInfoTool
+# from tools.place_search_tool import PlaceSearchTool
+# from tools.calculator_tool import CalculatorTool
+# from tools.currency_conversion_tool import CurrencyConversionTool
+
+##Langgraph imports
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolMode, tools_condition
+
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __call__(self):
+        pass
---
+++ utils/model_loader.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from typing import Any, Optional, Literal
+from pydantic import BaseModel, Field
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from utils.config_loader import load_config
+
+
+class ConfigLoader:
+    def __init__(self):
+        print("Initializing ConfigLoader...")
+        self.config = self.load_config()
+        print("ConfigLoader initialized.")
+
+    def __getitem__(self, key):
+        return self.config[key]
+
+class ModelLoader:
+    model_provider: Literal["groq", "openai"] = "groq"
+    config: Optional[ConfigLoader] = Field(default=None, exclude=True)
+
+    def model_post_init(self, __context: Any) -> None:
+        self.config = ConfigLoader()  # Initialize the config loader
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def load_llm(self):
+        """
+        Load and return the LLM model based on the specified provider.
+        """
+        print("LLM loading...")
+        print(f"Model provider: {self.model_provider}")
+        if self.model_provider == "groq":
+            print("Loading Groq LLM...")
+            groq_api_key = os.getenv("GROQ_API_KEY")
+            model_name = self.config["llm"]["groq"]["model_name"]
+            llm = ChatGroq(model=model_name, api_key=groq_api_key)
+            print("Groq LLM loaded successfully.")
+        elif self.model_provider == "openai":
+            print("Loading OpenAI LLM...")
+            openai_api_key = os.getenv("OPENAI_API_KEY")
+            model_name = self.config["llm"]["openai"]["model_name"]
+            llm = ChatOpenAI(model=model_name, openai_api_key=openai_api_key)
+            print("OpenAI LLM loaded successfully.")
+
+        return llm
